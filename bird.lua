b7a0399e9b22f75f210add15d63a9d2f91248f04
Fernando-López <flopezlopez6@outlook.com>
2021-03-03 12:04:12 -0600

    Bird restablished

--- bird.lua
+++ bird.lua
@@ -15,6 +15,7 @@ local timePassedSinceLastFrame = 0
 local flap = love.audio.newSource('/assets/audio/flap.wav', 'stream')
 
 function Bird:init()
+    math.randomseed(os.time())
     -- Load image
     selection = math.random(1, 3)
     if selection == 1 then
@@ -32,8 +33,17 @@ function Bird:init()
         for frame = 1, totalNumberofFrames do
             birdFrames[frame] = love.graphics.newQuad((frame - 1) * BIRD_WITDH, 0, BIRD_WITDH, BIRD_HEIGHT, birdImage:getDimensions())
         end
-    end    
-    return false
+    end
+    self.witdh = WITDH
+    self.height = HEIGHT
+
+    -- Displays bird in the middle of the screen
+    self.x = VIRTUAL_WIDTH / 2 - (BIRD_WITDH / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (BIRD_HEIGHT / 2)
+
+    -- y-axis velocity
+    self.dy = 0
+    
 end
 
 function Bird:update(dt)
@@ -56,3 +66,21 @@ function Bird:update(dt)
     -- Changes the posotion of the bird
     self.y = self.y + self.dy
 end
+
+function Bird:render()
+    love.graphics.draw(birdImage, birdFrames[currentFrame], self.x, self.y)
+end
+
+function Bird:collides(pipe)
+    -- Shifts the hit box of the bird to give more opportunities to the user
+    -- X axis hitbox
+    -- left        right
+    if (self.x + 3) + (BIRD_WITDH - 8) >= pipe.x and self.x + 3 <= pipe.x + PIPE_WIDTH - 15 then
+        -- top         bottom
+        if (self.y + 2) + (BIRD_HEIGHT - 2) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+    end
+
+    return false
+end    
